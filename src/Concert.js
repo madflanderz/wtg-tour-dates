19c50a4b976438a68856090a66ebbb82cde9aadb
madflanderz <anders79@web.de>
2018-12-05 16:08:06 +0100

    use flexbox for concerts

--- src/Concert.js
+++ src/Concert.js
@@ -2,17 +2,39 @@ import React from "react"
 import styled from "styled-components"
 
 const SConcert = styled.div`
-  display: grid;
+  display: flex;
   position: relative;
   max-width: 600px;
   margin: auto;
-  grid-template-columns: 100px 150px auto 70px;
+  text-align: left;
+  margin-bottom: 2rem;
+  flex-wrap: wrap;
+
   font-family: "Kaushan Script", cursive;
   font-size: 1.2rem;
   font-weight: bold;
-  span {
-    text-align: left;
-    margin-bottom: 30px;
+
+  .date {
+    width: 7rem;
+  }
+
+  .city {
+    width: 10rem;
+  }
+
+  .location {
+    flex: 1;
+  }
+
+  @media (max-width: 1000px) {
+    margin-bottom: 3rem;
+    max-width: 400px;
+    .date {
+      width: 40%;
+    }
+    .city {
+      width: 60%;
+    }
   }
 
   a {
@@ -22,34 +44,33 @@ const SConcert = styled.div`
 `
 
 const SoldOut = styled.div`
-  // width: 60px;
-  // height: 60px;
-  border-radius: 30px;
   text-shadow: 2px 2px 2px #a40a05;
   position: absolute;
   top: -0px;
   right: 0;
-  // background-color: red;
-  // border: 1px solid white;
   transform: rotate(10deg);
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @media (max-width: 1000px) {
+    position: initial;
+  }
 `
 
 const Concert = ({ date, city, location, ticketLink, soldOut }) => (
   <SConcert>
-    <span>{date}</span>
-    <span>{city}</span>
-    <span>{location}</span>
-    <span>
+    <div className="date">{date}</div>
+    <div className="city">{city}</div>
+    <div className="location">{location}</div>
+    <div className="tickets">
       {ticketLink && !soldOut && (
         <a href={ticketLink} target="_blank" rel="noopener">
           Tickets
         </a>
       )}
       {soldOut && <SoldOut>SOLD OUT</SoldOut>}
-    </span>
+    </div>
   </SConcert>
 )
 
